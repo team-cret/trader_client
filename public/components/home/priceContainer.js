f9551cbe59811b10cbb3ff3b0c204b73f81e26dd
HyeonJunSong <music9801@naver.com>
2023-12-24 21:10:15 +0900

    feat: price container ui

--- public/components/home/priceContainer.js
+++ public/components/home/priceContainer.js
@@ -1,55 +1,58 @@
-import { primary_dark, primary_light } from "@/public/assets/color";
+import { blue, primary_dark, primary_light, red } from "@/public/assets/color";
 import { SizedBox } from "../public/containers";
+import { formatPrice, formatRate } from "../../api/formatters";
 
 export default function PriceContainer() {
-  const priceInfo = [
-    {
-      status: "sell",
-      amount: 43,
-      price: 75,
-      rate: "-0.05%",
-    },
-    {
-      status: "sell",
-      amount: "1,000",
-      price: "50,000",
-      rate: "+1.72%",
-    },
-    {
-      status: "up",
-      amount: "1,000",
-      price: "50,000",
-      rate: "+1.72%",
-    },
-    {
-      status: "up",
-      amount: "1,000",
-      price: "50,000",
-      rate: "+1.72%",
-    },
-    {
-      status: "up",
-      amount: "1,000",
-      price: "50,000",
-      rate: "+1.72%",
-    },
-    {
-      status: "up",
-      amount: "1,000",
-      price: "50,000",
-      rate: "+1.72%",
-    },
-  ];
+  const priceInfo = {
+    totAmount: 529,
+    infos: [
+      {
+        status: "sell",
+        amount: 43,
+        price: 50500,
+        diffRate: -0.05,
+      },
+      {
+        status: "sell",
+        amount: 75,
+        price: 50400,
+        diffRate: -0.03,
+      },
+      {
+        status: "sell",
+        amount: 150,
+        price: 50200,
+        diffRate: -0.02,
+      },
+      {
+        status: "buy",
+        amount: 143,
+        price: 50200,
+        diffRate: 0.01,
+      },
+      {
+        status: "buy",
+        amount: 77,
+        price: 50100,
+        diffRate: 0.03,
+      },
+      {
+        status: "buy",
+        amount: 41,
+        price: 50000,
+        diffRate: 0.05,
+      },
+    ],
+  };
+
+  const pixPerAmount = 1;
 
   return (
     <div className="container">
       <div className="innerBox">
-        <PriceBox />
-        <PriceBox />
-        <PriceBox />
-        <PriceBox />
-        <PriceBox />
-        <PriceBox />
+        {priceInfo.infos.map((info) => {
+          return <PriceBox info={info} totAmount={priceInfo.totAmount} />;
+        })}
       </div>
       <style jsx>{`
         .container {
@@ -83,14 +86,19 @@ export default function PriceContainer() {
   );
 }
 
-//info = {status, amount, price, rate}
-function PriceBox({ info, pixPerAmount }) {
+//info = {status, amount, price, diffRate}
+function PriceBox({ info, totAmount }) {
   return (
     <div className="container">
-      <div className="amoutBox"></div>
-      <div className="priceBox"></div>
+      <div className="amountBox">
+        <div className={info.status}>
+          <div className="background" />
+          <div className="text">{info.amount}</div>
+        </div>
+      </div>
+      <div className="priceBox">{formatPrice(info.price)}</div>
       <SizedBox width={50} />
-      <div className="diffRateBox"></div>
+      <div className="diffRateBox">{formatRate(info.diffRate)}</div>
       <style jsx>{`
         .container {
           width: 418px;
@@ -107,25 +115,75 @@ function PriceBox({ info, pixPerAmount }) {
           padding-right: 22px;
         }
 
-        .amoutBox {
+        .amountBox {
           width: 154px;
           height: 27px;
+        }
 
-          background-color: tomato;
+        .amountBox .sell {
+          width: 50%;
+          height: 27px;
+
+          position: relative;
+        }
+        .amountBox .sell .background {
+          position: absolute;
+          right: 0;
+
+          width: ${(info.amount / totAmount) * 77}px;
+          height: 27px;
+          background-color: ${blue}7F;
+        }
+        .amountBox .sell .text {
+          position: absolute;
+          right: 0;
+
+          color: ${primary_dark};
+          font-size: 20px;
+          font-weight: 400;
+        }
+
+        .amountBox .buy {
+          width: 50%;
+          height: 27px;
+
+          transform: translateX(100%);
+
+          position: relative;
+        }
+        .amountBox .buy .background {
+          position: absolute;
+          left: 0;
+
+          width: ${(info.amount / totAmount) * 77}px;
+          height: 27px;
+          background-color: ${red}7F;
+        }
+        .amountBox .buy .text {
+          position: absolute;
+          left: 0;
+
+          color: ${primary_dark};
+          font-size: 20px;
+          font-weight: 400;
         }
 
         .priceBox {
           width: 86px;
           height: 27px;
 
-          background-color: tomato;
+          color: ${primary_dark};
+          font-size: 20px;
+          font-weight: 400;
         }
 
         .diffRateBox {
           width: 86px;
           height: 27px;
 
-          background-color: tomato;
+          color: ${primary_dark};
+          font-size: 20px;
+          font-weight: 400;
         }
       `}</style>
     </div>
